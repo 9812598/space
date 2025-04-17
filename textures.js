4e4aca46e3b290a1e628275e38cc7c4f02ba2886
9812598 <9812598@gmail.com>
2025-04-17 15:16:54 +0300

    correct preview

--- textures.js
+++ textures.js
@@ -1,6 +1,6 @@
-﻿import spaceTextureUrl from "./space.jpg?url";
-import moonTextureUrl from "./moon.jpg?url";
-import normalTextureUrl from "./normal.jpg?url";
-import kobelevTextureUrl from "./kobelev.png?url";
+﻿import spaceTextureUrl from "./public/images/space.jpg?url";
+import moonTextureUrl from "./public/images/moon.jpg?url";
+import normalTextureUrl from "./public/images/normal.jpg?url";
+import kobelevTextureUrl from "./public/images/kobelev.png?url";
 
 export { spaceTextureUrl, moonTextureUrl, normalTextureUrl, kobelevTextureUrl };
