b8c8f1a2f7b0aeadc874e30a71fb8c5e1a5bcff8
9812598 <9812598@gmail.com>
2025-04-17 16:38:32 +0300

    correct mobile

--- textures.js
+++ textures.js
@@ -1,6 +1,6 @@
-﻿import spaceTextureUrl from "./public/images/space.jpg";
-import moonTextureUrl from "./public/images/moon.jpg";
-import normalTextureUrl from "./public/images/normal.jpg";
-import kobelevTextureUrl from "./public/images/kobelev.png";
+﻿import spaceTextureUrl from "./public/space.jpg?url";
+import moonTextureUrl from "./public/moon.jpg?url";
+import normalTextureUrl from "./public/normal.jpg?url";
+import kobelevTextureUrl from "./public/kobelev.png?url";
 
 export { spaceTextureUrl, moonTextureUrl, normalTextureUrl, kobelevTextureUrl };
